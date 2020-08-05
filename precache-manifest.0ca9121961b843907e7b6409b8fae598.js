18b80a694f39039c32cdd5d3c43c6ead5862f199
Sidney <Hyosang@Macintoshui-MacBook-Pro.local>
2020-08-05 16:25:21 +0900

    Updates

---
+++ precache-manifest.0ca9121961b843907e7b6409b8fae598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963ba103482973211a8b61ce817ab613",
+    "url": "/reactbasic/index.html"
+  },
+  {
+    "revision": "0ced94f745190da0e0ed",
+    "url": "/reactbasic/static/css/main.eeae668a.chunk.css"
+  },
+  {
+    "revision": "ab788872ceaa638d7694",
+    "url": "/reactbasic/static/js/2.a5784b9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactbasic/static/js/2.a5784b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ced94f745190da0e0ed",
+    "url": "/reactbasic/static/js/main.2abda86c.chunk.js"
+  },
+  {
+    "revision": "619319380afcd44a2b6b",
+    "url": "/reactbasic/static/js/runtime-main.691204dd.js"
+  }
+]);
